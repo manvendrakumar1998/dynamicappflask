750829071e262929181d28cf75fdaaf2453b9063
manvendra1998 <manvendra.bihar.bharat@gmail.com>
2023-12-07 15:34:42 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return "Flask app that uses URL parameters to display dynamic content."
+
+@app.route("/dynamic")
+def dynamic_content():
+    name = request.args.get("name","Guest")
+    return f'Hi {name} this is dynamic content'
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
